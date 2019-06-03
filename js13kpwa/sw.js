38f060974b4e2ecc5098c6489ea5a9ed116ef1c1
chlwogns72 <wogns083@gmail.com>
2019-06-03 17:13:41 +0900

    PWA - change wrong path3

--- js13kpwa/sw.js
+++ js13kpwa/sw.js
@@ -3,25 +3,26 @@ self.importScripts('data/games.js');
 // Files to cache
 var cacheName = 'js13kPWA-v1';
 var appShellFiles = [
-  '/pwa-examples/js13kpwa/',
-  '/pwa-examples/js13kpwa/index.html',
-  '/pwa-examples/js13kpwa/app.js',
-  '/pwa-examples/js13kpwa/style.css',
-  '/pwa-examples/js13kpwa/fonts/graduate.eot',
-  '/pwa-examples/js13kpwa/fonts/graduate.ttf',
-  '/pwa-examples/js13kpwa/fonts/graduate.woff',
-  '/pwa-examples/js13kpwa/favicon.ico',
-  '/pwa-examples/js13kpwa/img/js13kgames.png',
-  '/pwa-examples/js13kpwa/img/bg.png',
-  '/pwa-examples/js13kpwa/icons/icon-32.png',
-  '/pwa-examples/js13kpwa/icons/icon-64.png',
-  '/pwa-examples/js13kpwa/icons/icon-96.png',
-  '/pwa-examples/js13kpwa/icons/icon-128.png',
-  '/pwa-examples/js13kpwa/icons/icon-168.png',
-  '/pwa-examples/js13kpwa/icons/icon-192.png',
-  '/pwa-examples/js13kpwa/icons/icon-256.png',
-  '/pwa-examples/js13kpwa/icons/icon-512.png'
+  '/vue-example/js13kpwa/',
+  '/vue-example/js13kpwa/index.html',
+  '/vue-example/js13kpwa/app.js',
+  '/vue-example/js13kpwa/astyle.css',
+  '/vue-example/js13kpwa/fonts/graduate.eot',
+  '/vue-example/js13kpwa/fonts/graduate.ttf',
+  '/vue-example/js13kpwa/fonts/graduate.woff',
+  '/vue-example/js13kpwa/favicon.ico',
+  '/vue-example/js13kpwa/img/js13kgames.png',
+  '/vue-example/js13kpwa/img/bg.png',
+  '/vue-example/js13kpwa/icons/icon-32.png',
+  '/vue-example/js13kpwa/icons/icon-64.png',
+  '/vue-example/js13kpwa/icons/icon-96.png',
+  '/vue-example/js13kpwa/icons/icon-128.png',
+  '/vue-example/js13kpwa/icons/icon-168.png',
+  '/vue-example/js13kpwa/icons/icon-192.png',
+  '/vue-example/js13kpwa/icons/icon-256.png',
+  '/vue-example/js13kpwa/icons/icon-512.png'
 ];
+
 var gamesImages = [];
 for(var i=0; i<games.length; i++) {
   gamesImages.push('data/img/'+games[i].slug+'.jpg');
