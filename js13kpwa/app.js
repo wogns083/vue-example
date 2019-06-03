9faa7b941d58a8c1ceacfd3bbebd50acc4317ae5
chlwogns72 <wogns083@gmail.com>
2019-06-03 17:00:17 +0900

    PWA - change a wrong path2

--- js13kpwa/app.js
+++ js13kpwa/app.js
@@ -27,7 +27,7 @@ document.getElementById('content').innerHTML = content;
 // Registering Service Worker
 if('serviceWorker' in navigator) {
 	// navigator.serviceWorker.register('/pwa-examples-master/js13kpwa/sw.js');
-	navigator.serviceWorker.register('./sw.js');
+	navigator.serviceWorker.register('sw.js');
 };
 
 // Requesting permission for Notifications after clicking on the button
